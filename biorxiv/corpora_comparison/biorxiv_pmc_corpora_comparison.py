9eccc7846ee80634f587ddd65b99597a2ce29b01
David Nicholson <dnicholson329@gmail.com>
2020-05-20 16:44:13 -0400

    Updated analysis to all preprints vs published articles

--- biorxiv/corpora_comparison/biorxiv_pmc_corpora_comparison.py
+++ biorxiv/corpora_comparison/biorxiv_pmc_corpora_comparison.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Comparative Linguistic Analysis of bioRxiv and PMC
+
+# In[1]:
+
+
 from collections import defaultdict, Counter
 import csv
 from pathlib import Path
@@ -8,6 +16,10 @@ import pickle
 import spacy
 from scipy.stats import chi2_contingency
 from tqdm import tqdm_notebook
+
+
+# In[2]:
+
 
 def get_term_statistics(corpus_one, corpus_two, term, psudeocount=1, eps=1e-20):
     """
@@ -66,6 +78,10 @@ def get_term_statistics(corpus_one, corpus_two, term, psudeocount=1, eps=1e-20):
         "odds_ratio":log_ratio
     }
 
+
+# In[3]:
+
+
 def aggregate_word_counts(doc_iterator):
     global_word_counter = Counter()
     
@@ -78,6 +94,10 @@ def aggregate_word_counts(doc_iterator):
             })
 
     return global_word_counter
+
+
+# In[4]:
+
 
 def remove_stop_words(corpus_one, corpus_two):
     spacy_nlp = spacy.load('en_core_web_sm')
@@ -93,11 +113,23 @@ def remove_stop_words(corpus_one, corpus_two):
             
     return corpus_one, corpus_two
 
+
+# # Full Text Comparison (Global)
+
+# ## Gather Word Frequencies
+
+# In[4]:
+
+
 biorxiv_corpus_count = (
     aggregate_word_counts(
         list(Path("output/biorxiv_word_counts").rglob("*tsv"))
     )
 )
+
+
+# In[5]:
+
 
 pmc_corpus_count = (
     aggregate_word_counts(
@@ -105,44 +137,94 @@ pmc_corpus_count = (
     )
 )
 
+
+# In[8]:
+
+
 biorxiv_corpus_count.most_common(10)
 
+
+# In[9]:
+
+
 pmc_corpus_count.most_common(10)
+
+
+# In[6]:
+
 
 pickle.dump(biorxiv_corpus_count, open("output/biorxiv_total_count.pkl", "wb"))
 pickle.dump(pmc_corpus_count, open("output/pmc_total_count.pkl", "wb"))
 
+
+# ## Analysis without Stop Words
+
+# In[5]:
+
+
 biorxiv_corpus_count = pickle.load(open("output/biorxiv_total_count.pkl", "rb"))
 pmc_corpus_count = pickle.load(open("output/pmc_total_count.pkl", "rb"))
+
+
+# In[6]:
+
 
 biorxiv_corpus_count, pmc_corpus_count = remove_stop_words(
     biorxiv_corpus_count,
     pmc_corpus_count
 )
 
+
+# In[7]:
+
+
 top_ten_biorxiv = biorxiv_corpus_count.most_common(100)
 top_ten_biorxiv[0:10]
+
+
+# In[8]:
+
 
 top_ten_pmc = pmc_corpus_count.most_common(100)
 top_ten_pmc[0:10]
 
+
+# In[9]:
+
+
 print("Number of words in biorxiv but not in Pubmed Central:")
 biorxiv_difference = set(list(biorxiv_corpus_count.keys())) - set(list(pmc_corpus_count.keys()))
 print(len(biorxiv_difference))
+
+
+# In[10]:
+
 
 [
     biorxiv_difference.pop()
     for i in range(10)
 ]
 
+
+# In[11]:
+
+
 print("Number of words in Pubmed Central but not in biorxiv:")
 pmc_difference = set(list(pmc_corpus_count.keys())) - set(list(biorxiv_corpus_count.keys()))
 print(len(pmc_difference))
+
+
+# In[12]:
+
 
 [
     pmc_difference.pop()
     for i in range(10)
 ]
+
+
+# In[13]:
+
 
 total_words = set(list(dict(top_ten_biorxiv).keys()) + list(dict(top_ten_pmc).keys()))
 data = []
@@ -165,6 +247,10 @@ for word in tqdm_notebook(total_words):
         "odds_ratio": word_stat['odds_ratio']
     })
 
+
+# In[14]:
+
+
 total_word_stats_df = pd.DataFrame.from_records(data)
 total_word_stats_df.to_csv(
     "output/full_corpus_comparison_stats.tsv", 
@@ -172,17 +258,31 @@ total_word_stats_df.to_csv(
 )
 total_word_stats_df.head()
 
+
+# In[15]:
+
+
 (
     total_word_stats_df
     .sort_values("log_likelihood", ascending=False)
     .head(20)
 )
 
+
+# In[16]:
+
+
 (
     total_word_stats_df
     .sort_values("odds_ratio", ascending=False)
     .head(20)
 )
+
+
+# # Preprint to Published View
+
+# In[17]:
+
 
 mapped_doi_df = (
     pd.read_csv("../journal_tracker/output/mapped_published_doi.tsv", sep="\t")
@@ -193,7 +293,7 @@ mapped_doi_df = (
         "author_type":"first",
         "heading":"first",
         "category":"first",
-        "document":"last",
+        "document":"first",
         "doi":"last",
         "published_doi":"last",
         "journal":"last",
@@ -203,44 +303,95 @@ mapped_doi_df = (
 )
 mapped_doi_df.tail()
 
-preprint_count = aggregate_word_counts(
-    [Path("output/biorxiv_word_counts/862847_v1.tsv")]
-)
 
-published_count = aggregate_word_counts(
-    [Path("output/pmc_word_counts/PMC6933653.tsv")]
-)
+# In[18]:
+
+
+print(f"Total # of Preprints Mapped: {mapped_doi_df.shape[0]}")
+print(f"Total % of Mapped: {mapped_doi_df.shape[0]/71118}")
+
+
+# In[19]:
+
+
+preprint_count = aggregate_word_counts([
+    Path(f"output/biorxiv_word_counts/{Path(file).stem}.tsv")
+    for file in mapped_doi_df.document.values.tolist()
+    if Path(f"output/biorxiv_word_counts/{Path(file).stem}.tsv").exists()
+])
+
+
+# In[20]:
+
+
+published_count = aggregate_word_counts([
+    Path(f"output/pmc_word_counts/{file}.tsv")
+    for file in mapped_doi_df.pmcid.values.tolist()
+    if Path(f"output/pmc_word_counts/{file}.tsv").exists()
+])
+
+
+# In[21]:
+
 
 preprint_count, published_count = remove_stop_words(
     preprint_count,
     published_count
 )
 
-top_ten_preprint = preprint_count.most_common(100)
-top_ten_preprint[0:10]
 
-top_ten_published = published_count.most_common(100)
-top_ten_published[0:10]
+# In[22]:
+
+
+top_hundred_preprint = preprint_count.most_common(100)
+top_hundred_preprint[0:10]
+
+
+# In[23]:
+
+
+top_hundred_published = published_count.most_common(100)
+top_hundred_published[0:10]
+
+
+# In[24]:
+
 
 print("Number of words in preprint but not in published version:")
 preprint_difference = set(list(preprint_count.keys())) - set(list(published_count.keys()))
 print(len(preprint_difference))
+
+
+# In[25]:
+
 
 [
     preprint_difference.pop()
     for i in range(10)
 ]
 
+
+# In[26]:
+
+
 print("Number of words in published version but not in preprint:")
 published_difference = set(list(published_count.keys())) - set(list(preprint_count.keys()))
 print(len(published_difference))
+
+
+# In[27]:
+
 
 [
     published_difference.pop()
     for i in range(10)
 ]
 
-total_words = set(list(dict(top_ten_preprint).keys()) + list(dict(top_ten_published).keys()))
+
+# In[28]:
+
+
+total_words = set(list(dict(top_hundred_preprint).keys()) + list(dict(top_hundred_published).keys()))
 data = []
 for word in tqdm_notebook(total_words):
     
@@ -261,12 +412,20 @@ for word in tqdm_notebook(total_words):
         "odds_ratio": word_stat['odds_ratio']
     })
 
+
+# In[29]:
+
+
 published_comparison_stats_df = pd.DataFrame.from_records(data)
 published_comparison_stats_df.to_csv(
-    "output/544536_v2_PMC6687187_comparison.tsv", 
+    "output/preprint_to_published_comparison.tsv", 
     sep="\t", index=False
 )
 published_comparison_stats_df.head()
+
+
+# In[30]:
+
 
 (
     published_comparison_stats_df
@@ -274,8 +433,13 @@ published_comparison_stats_df.head()
     .head(20)
 )
 
+
+# In[31]:
+
+
 (
     published_comparison_stats_df
     .sort_values("odds_ratio", ascending=False)
     .head(20)
 )
+
