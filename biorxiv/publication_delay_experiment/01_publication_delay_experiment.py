92cdcea2bbeddee039680b02fefa05cea52ce4a0
David Nicholson <dnicholson329@gmail.com>
2021-04-21 16:28:21 -0400

    added figures to repository

---
+++ biorxiv/publication_delay_experiment/01_publication_delay_experiment.py
@@ -0,0 +1,390 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.9.1+dev
+#   kernelspec:
+#     display_name: annorxiver
+#     language: python
+#     name: python3
+# ---
+
+# # Measure the Difference between Preprint-Published similarity and Published Articles
+
+# This notebook measures the time delay that results from the peer review process. Two plots are generated: one that depict the average publication time delay as changes are demanded from the peer review process and the other that depicts the added time delay as preprints have to undergo multiple versions to be published.
+
+# +
+from datetime import timedelta
+import random
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import plotnine as p9
+import requests
+from scipy.spatial.distance import cdist
+from scipy.stats import linregress
+import seaborn as sns
+from sklearn.linear_model import LogisticRegressionCV
+import tqdm
+
+from mizani.breaks import date_breaks
+from mizani.formatters import timedelta_format, log_format
+# -
+
+# # Gather Preprint-Published Pairs
+
+# ## bioRxiv Document Embeddings
+
+url = "https://api.biorxiv.org/pub/2000-01-01/3000-01-01/"
+
+# +
+already_downloaded = Path("output/biorxiv_published_dates.tsv").exists()
+if not already_downloaded:
+    collection = []
+    page_size = 100
+    total = 44397
+    for i in tqdm.tqdm(range(0, total, page_size), total=total / page_size):
+        collection += requests.get(url + str(i)).json()["collection"]
+    published_dates = pd.DataFrame(collection)
+    published_dates.to_csv("output/biorxiv_published_dates.tsv", sep="\t", index=False)
+else:
+    published_dates = pd.read_csv("output/biorxiv_published_dates.tsv", sep="\t")
+
+print(published_dates.shape)
+published_dates.head()
+# -
+
+biorxiv_journal_df = (
+    pd.read_csv("../journal_tracker/output/mapped_published_doi.tsv", sep="\t")
+    .groupby("preprint_doi")
+    .agg(
+        {
+            "document": "first",
+            "category": "first",
+            "preprint_doi": "count",
+            "published_doi": "first",
+            "pmcid": "first",
+            "pmcoa": "first",
+        }
+    )
+    .rename(index=str, columns={"preprint_doi": "version_count"})
+    .reset_index()
+)
+print(biorxiv_journal_df.shape)
+biorxiv_journal_df.head()
+
+biorxiv_embed_df = pd.read_csv(
+    Path("../word_vector_experiment/output/")
+    / "word2vec_output/"
+    / "biorxiv_all_articles_300_fixed.tsv.xz",
+    sep="\t",
+)
+biorxiv_embed_df = biorxiv_embed_df.dropna()
+print(biorxiv_embed_df.shape)
+biorxiv_embed_df.head()
+
+# ## PMC Article Embeddings
+
+pmc_articles_df = pd.read_csv(
+    Path("../../pmc/exploratory_data_analysis/")
+    / "output/pubmed_central_journal_paper_map.tsv.xz",
+    sep="\t",
+).query("article_type=='research-article'")
+print(pmc_articles_df.shape)
+pmc_articles_df.head()
+
+pmc_embed_df = pd.read_csv(
+    Path("../../pmc/word_vector_experiment/output")
+    / Path("pmc_document_vectors_300_replace.tsv.xz"),
+    sep="\t",
+)
+pmc_embed_df.head()
+
+biorxiv_journal_df = biorxiv_journal_df.merge(
+    pmc_articles_df[["journal", "pmcid"]], on="pmcid", how="left"
+)
+print(biorxiv_journal_df.shape)
+biorxiv_journal_df.head()
+
+# # Gather Golden bioRxiv Set
+
+matched_preprint_published_pairs = (
+    biorxiv_journal_df.query("pmcoa==True")
+    .sort_values("document")
+    .merge(
+        published_dates[["biorxiv_doi", "preprint_date", "published_date"]].rename(
+            index=str, columns={"biorxiv_doi": "preprint_doi"}
+        ),
+        on="preprint_doi",
+    )
+    .assign(
+        preprint_date=lambda x: pd.to_datetime(x.preprint_date.tolist()),
+        published_date=lambda x: pd.to_datetime(
+            x.published_date.apply(lambda y: y[0 : y.index(":")] if ":" in y else y)
+        ),
+    )
+    .assign(time_to_published=lambda x: x.published_date - x.preprint_date)
+)
+print(matched_preprint_published_pairs.shape)
+matched_preprint_published_pairs.head()
+
+# # Calculate the Document Distances
+
+# This block calculates the euclidean distance between preprint's first version and their final published version.
+
+biorxiv_documents = (
+    biorxiv_embed_df.query(
+        f"document in {matched_preprint_published_pairs.document.tolist()}"
+    )
+    .set_index("document")
+    .reindex(matched_preprint_published_pairs.document.tolist())
+    .fillna(0)
+)
+biorxiv_documents.head()
+
+pmc_documents = (
+    pmc_embed_df.query(f"document in {matched_preprint_published_pairs.pmcid.tolist()}")
+    .set_index("document")
+    .reindex(matched_preprint_published_pairs.pmcid.tolist())
+    .drop("journal", axis=1)
+    .fillna(0)
+)
+pmc_documents.head()
+
+# +
+published_date_distances = (
+    matched_preprint_published_pairs.assign(
+        doc_distances=np.diag(
+            cdist(biorxiv_documents.values, pmc_documents.values, "euclidean")
+        )
+    )
+    .replace(0, np.nan)
+    .dropna()
+    .query("doc_distances.notnull()")
+)
+
+published_date_distances.to_csv(
+    "output/preprint_published_distances_rerun.tsv", sep="\t", index=False
+)
+
+print(published_date_distances.shape)
+published_date_distances.head()
+# -
+
+# # Construct Scatter Plot of Date vs Version Count
+
+# Preprints are delayed on an average of 51 days for each new version posted onto bioRxiv. This section regresses preprint's version counts against the time it takes to have a preprint published. A scatter and square bin plot are generated below.
+
+# +
+# Get smoothed linear regression line
+x = published_date_distances.version_count.values.tolist()
+
+y = published_date_distances.time_to_published.apply(
+    lambda x: x / timedelta(days=1)
+).tolist()
+
+xseq_2 = np.linspace(np.min(x), np.max(x), 80)
+
+results_2 = linregress(x, y)
+print(results_2)
+# -
+
+g = (
+    p9.ggplot(
+        published_date_distances,
+        p9.aes(x="factor(version_count)", y="time_to_published"),
+    )
+    + p9.geom_boxplot(fill="#a6cee3")
+    + p9.geom_line(
+        mapping=p9.aes(x="version_count", y="time_to_published"),
+        stat="smooth",
+        method="lm",
+        linetype="dashed",
+        se=False,
+        alpha=1,
+        size=0.7,
+        inherit_aes=False,
+    )
+    + p9.scale_y_timedelta(labels=timedelta_format("d"))
+    + p9.annotate(
+        "text",
+        x=9,
+        y=timedelta(days=1470),
+        label=f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
+    )
+    + p9.labs(x="# of Preprint Versions", y="Time Elapsed Until Preprint is Published")
+    + p9.theme_seaborn(context="paper", style="ticks", font="Arial", font_scale=1.3)
+)
+g.save("output/version_count_vs_publication_time_rerun.svg", dpi=500)
+g.save("output/version_count_vs_publication_time_rerun.png", dpi=500)
+print(g)
+
+# # Construct Scatter Plot of Date vs Document Distances
+
+# Preprints are delayed on an average of 17 days as changes are demanded from the peer-review process. This section regresses a preprint's document distance against the time it takes to have a preprint published. A scatter and square bin plot are generated below.
+
+# +
+# Get smoothed linear regression line
+# Was Performed with Negative time added
+x = published_date_distances.doc_distances.values.tolist()
+
+y = published_date_distances.time_to_published.apply(
+    lambda x: x / timedelta(days=1)
+).tolist()
+
+xseq_2 = np.linspace(np.min(x), np.max(x), 80)
+
+results_2 = linregress(x, y)
+print(results_2)
+# -
+
+g = (
+    p9.ggplot(
+        published_date_distances, p9.aes(y="time_to_published", x="doc_distances")
+    )
+    + p9.geom_point()
+    + p9.geom_line(
+        stat="smooth", method="lm", linetype="dashed", se=False, alpha=0.9, size=0.6
+    )
+    + p9.scale_y_timedelta(labels=timedelta_format("d"))
+    + p9.annotate(
+        "text",
+        x=10,
+        y=timedelta(days=1450),
+        label=f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
+    )
+    + p9.labs(
+        x="Eucledian Distance of Preprints First and Final Versions",
+        y="Time Elapsed Until Preprint is Published",
+    )
+    + p9.theme_seaborn(context="paper", style="ticks", font="Arial", font_scale=1.3)
+)
+print(g)
+
+g = (
+    p9.ggplot(
+        published_date_distances, p9.aes(x="doc_distances", y="time_to_published")
+    )
+    + p9.geom_bin2d(bins=100)
+    + p9.scale_fill_distiller(
+        trans="log",
+        direction=-1,
+        type="seq",
+        palette="YlGnBu",
+        name="log(count)",
+        labels=log_format(base=10),
+    )
+    + p9.geom_line(
+        stat="smooth", method="lm", linetype="dashed", se=False, alpha=1, size=0.7
+    )
+    + p9.scale_y_timedelta(labels=timedelta_format("d"))
+    + p9.annotate(
+        "text",
+        x=10,
+        y=timedelta(days=1490),
+        label=f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
+    )
+    + p9.labs(
+        x="Eucledian Distance of Preprint-Published Versions",
+        y="Time Elapsed Until Preprint is Published",
+        legend="log(count)",
+    )
+)
+g.save("output/article_distance_vs_publication_time_rerun.svg", dpi=250)
+g.save("output/article_distance_vs_publication_time_rerun.png", dpi=250)
+print(g)
+
+
+# # Contextualize Document Distances
+
+# The goal here is to understand what a unit of distance represents for two document embeddings. It is already established that low distances can indicate similar documents, but question remains how does a unit of distances relate to time taken to get published? To answer this question I randomly sampled two preprints from the following groups: same journal, same preprint category and conglomeration of all bioRxiv preprints. Sampled preprints have their distance measured and I report the average distance of each group.
+
+# +
+def random_combination(iterable, r, size=100, seed=100):
+    "Random selection from itertools.combinations(iterable, r)"
+    random.seed(seed)
+
+    pool = tuple(iterable)
+    n = len(pool)
+
+    indices = [sorted(random.sample(range(n), r)) for elem in range(size)]
+
+    for i in indices:
+        yield (pool[i[0]], pool[i[1]])
+
+
+def article_distances(
+    iterable, embed_df, combination_size=2, sample_size=100, seed=100
+):
+    article_pair_generator = random_combination(
+        iterable, combination_size, sample_size, seed
+    )
+
+    paper_one, paper_two = zip(*article_pair_generator)
+    temp_index = embed_df.set_index("document")
+
+    return np.diag(
+        cdist(
+            temp_index.loc[list(paper_one)].values,
+            temp_index.loc[list(paper_two)].values,
+            "euclidean",
+        )
+    )
+
+
+# +
+# Randomly sample two papers from the same journal 1000 times - plos one
+plos_one_distances = article_distances(
+    biorxiv_journal_df.query("journal=='PLoS_Genet'").document.tolist(),
+    biorxiv_embed_df,
+    2,
+    1000,
+)
+
+print(
+    f"Genetics Stats Mean: {np.mean(plos_one_distances):.3f}, "
+    f"Std: {np.std(plos_one_distances):.3f}"
+)
+
+# +
+# Randomly sample two papers from the same field category 1000 times - bioinformatics
+bioinformatic_distances = article_distances(
+    biorxiv_journal_df.query("category == 'bioinformatics'")
+    .query(f"document in {biorxiv_embed_df.document.tolist()}")
+    .document.tolist(),
+    biorxiv_embed_df,
+    2,
+    1000,
+)
+
+print(
+    f"Bioinformatics Stats Mean: {np.mean(bioinformatic_distances):.3f}, "
+    f"Std: {np.std(bioinformatic_distances):.3f}"
+)
+
+# +
+# Randomly sample two papers from the entire bioRxiv corpus 1000 times
+biorxiv_distances = article_distances(
+    biorxiv_journal_df.query(
+        f"document in {biorxiv_embed_df.document.tolist()}"
+    ).document.tolist(),
+    biorxiv_embed_df,
+    2,
+    1000,
+)
+
+print(
+    f"Biorxiv Stats Mean: {np.mean(biorxiv_distances):.3f}, "
+    f"Std: {np.std(biorxiv_distances):.3f}"
+)
+# -
+
+# Take home results:
+#     1. It takes approximately 51 days for a new preprint version to be posted onto bioRxiv.
+#     2. Making peer review changes takes on average 17 days to make for given preprints.
+#     3. A distance unit reflects about an 18% ((6.210-5.068)/6.210) change of a preprints textual content
--- biorxiv/publication_delay_experiment/02_publication_delay_experiment_figure_exploration.py
+++ biorxiv/publication_delay_experiment/02_publication_delay_experiment_figure_exploration.py
@@ -1,13 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.9.1+dev
+#   kernelspec:
+#     display_name: Python [conda env:annorxiver]
+#     language: python
+#     name: conda-env-annorxiver-py
+# ---
 
 # # Measure the Difference between Preprint-Published similarity and Published Articles
 
 # This notebook measures the time delay that results from the peer review process. Two plots are generated: one that depict the average publication time delay as changes are demanded from the peer review process and the other that depicts the added time delay as preprints have to undergo multiple versions to be published.
 
-# In[1]:
-
-
+# +
 from datetime import timedelta
 import random
 from pathlib import Path
@@ -15,9 +25,6 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.patches import ConnectionPatch
-
-mpl.rcParams["figure.dpi"] = 250
-
 import numpy as np
 import pandas as pd
 import plotnine as p9
@@ -33,16 +40,14 @@ from mizani.formatters import timedelta_format
 
 import warnings
 
+mpl.rcParams["figure.dpi"] = 250
 warnings.filterwarnings("ignore")
-
+# -
 
 # # Load the Document Distances
 
-# In[2]:
-
-
 published_date_distances = pd.read_csv(
-    "../../pmc/word_vector_experiment/output/preprint_published_distances.csv"
+    "output/preprint_published_distances_rerun.tsv", sep="\t"
 )
 for col in ["preprint_date", "published_date"]:
     published_date_distances[col] = pd.to_datetime(published_date_distances[col])
@@ -52,17 +57,9 @@ published_date_distances["time_to_published"] = pd.to_timedelta(
 print(published_date_distances.shape)
 published_date_distances.head()
 
-
-# In[3]:
-
-
 published_date_distances["days_to_published"] = published_date_distances[
     "time_to_published"
 ].dt.days
-
-
-# In[4]:
-
 
 remove_negative_time_to_published = True
 if remove_negative_time_to_published:
@@ -70,14 +67,11 @@ if remove_negative_time_to_published:
         published_date_distances["days_to_published"] >= 0
     ]
 
-
 # # Construct Scatter Plot of Date vs Version Count
 
 # Preprints are delayed on an average of 51 days for each new version posted onto bioRxiv. This section regresses preprint's version counts against the time it takes to have a preprint published. A scatter and square bin plot are generated below.
 
-# In[5]:
-
-
+# +
 # Get smoothed linear regression line
 x = published_date_distances.version_count.values.tolist()
 
@@ -89,10 +83,7 @@ xseq_2 = np.linspace(np.min(x), np.max(x), 80)
 
 results_2 = linregress(x, y)
 print(results_2)
-
-
-# In[6]:
-
+# -
 
 x_line = np.array(
     [
@@ -101,10 +92,6 @@ x_line = np.array(
     ]
 )
 y_line = x_line * results_2.slope + results_2.intercept
-
-
-# In[7]:
-
 
 g = (
     p9.ggplot(
@@ -136,10 +123,6 @@ g = (
 # g.save("output/version_count_vs_publication_time.png", dpi=500)
 print(g)
 
-
-# In[8]:
-
-
 plt.figure(figsize=(8, 5))
 g = sns.boxenplot(
     x="version_count",
@@ -152,10 +135,6 @@ _ = g.set_ylabel("Time Elapsed Until Preprint is Published (Days)")
 _ = g.set_xlabel("# of Preprint Versions")
 _ = g.plot(x_line - 1, y_line, "--k")
 _ = g.annotate(f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (7, 1470))
-
-
-# In[9]:
-
 
 plt.figure(figsize=(8, 5))
 g = sns.violinplot(
@@ -170,15 +149,12 @@ _ = g.set_ylabel("Time Elapsed Until Preprint is Published (Days)")
 _ = g.set_xlabel("# of Preprint Versions")
 _ = g.plot(x_line - 1, y_line, "--k")
 _ = g.annotate(f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (7, 1470))
-_ = g.set_xlim(-.5, 11.5)
+_ = g.set_xlim(-0.5, 11.5)
 _ = g.set_ylim(0, g.get_ylim()[1])
-plt.savefig("output/version_count_vs_publication_time_violin.svg", dpi=500)
-plt.savefig("output/version_count_vs_publication_time_violin.png", dpi=500)
+plt.savefig("output/version_count_vs_publication_time_violin_rerun.svg", dpi=500)
+plt.savefig("output/version_count_vs_publication_time_violin_rerun.png", dpi=500)
 
-
-# In[10]:
-
-
+# +
 sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
 
 bw_adjust = 0.5
@@ -262,10 +238,7 @@ _ = g.set_titles("")
 _ = g.set(xticks=[])
 _ = g.despine(bottom=True, left=True)
 
-
-# In[11]:
-
-
+# +
 sns.set_theme(style="white", rc={"axes.facecolor": (0, 0, 0, 0)})
 
 g = sns.FacetGrid(
@@ -288,9 +261,15 @@ g.map(
     linewidth=1.5,
 )
 g.map(
-    sns.kdeplot, "days_to_published", clip_on=False, color="w", lw=2, bw_adjust=0.5,
+    sns.kdeplot,
+    "days_to_published",
+    clip_on=False,
+    color="w",
+    lw=2,
+    bw_adjust=0.5,
 )
 g.map(plt.axhline, y=0, lw=2, clip_on=False)
+
 
 # Define and use a simple function to label the plot in axes coordinates
 def label(x, color, label):
@@ -318,10 +297,7 @@ g.fig.subplots_adjust(hspace=-0.7)
 g.set_titles("")
 g.set(yticks=[])
 g.despine(bottom=True, left=True)
-
-
-# In[12]:
-
+# -
 
 g = sns.lmplot(
     x="version_count",
@@ -332,19 +308,20 @@ g = sns.lmplot(
 )
 _ = g.set_ylabels("Time Elapsed Until Preprint is Published (Days)")
 _ = g.set_xlabels("# of Preprint Versions")
-_ = g.axes[0][0].annotate(f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (2, 700))
-_ = g.axes[0][0].set_xlim(.5, 12.5)
+_ = g.axes[0][0].annotate(
+    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (2, 700)
+)
+_ = g.axes[0][0].set_xlim(0.5, 12.5)
 _ = g.axes[0][0].set_ylim(0, g.axes[0][0].get_ylim()[1])
-
 
 # # Construct Scatter Plot of Date vs Document Distances
 
 # Preprints are delayed on an average of 17 days as changes are demanded from the peer-review process. This section regresses a preprint's document distance against the time it takes to have a preprint published. A scatter and square bin plot are generated below.
 
-# In[13]:
 
-
+# +
 # Get smoothed linear regression line
+# Removed negative time here
 x = published_date_distances.doc_distances.values.tolist()
 
 y = published_date_distances.time_to_published.apply(
@@ -355,10 +332,7 @@ xseq_2 = np.linspace(np.min(x), np.max(x), 80)
 
 results_2 = linregress(x, y)
 print(results_2)
-
-
-# In[14]:
-
+# -
 
 g = (
     p9.ggplot(
@@ -383,10 +357,6 @@ g = (
 )
 print(g)
 
-
-# In[15]:
-
-
 g = (
     p9.ggplot(
         published_date_distances, p9.aes(x="doc_distances", y="time_to_published")
@@ -401,7 +371,7 @@ g = (
     + p9.scale_y_timedelta(labels=timedelta_format("d"))
     + p9.annotate(
         "text",
-        x=15,
+        x=7.5,
         y=timedelta(days=1490),
         label=f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
     )
@@ -411,16 +381,10 @@ g = (
         legend="log(count)",
     )
 )
-# g.save("output/article_distance_vs_publication_time.svg", dpi=250)
-# g.save("output/article_distance_vs_publication_time.png", dpi=250)
 print(g)
-
 
 # # Hex grid options
 # A couple hex grid options just to see what they look like
-
-# In[16]:
-
 
 x_line = np.array(
     [
@@ -430,11 +394,7 @@ x_line = np.array(
 )
 y_line = x_line * results_2.slope + results_2.intercept
 
-
-# In[17]:
-
-
-plt.figure(figsize=(6, 5))
+plt.figure(figsize=(7, 5))
 ax = plt.hexbin(
     published_date_distances["doc_distances"],
     published_date_distances["days_to_published"],
@@ -445,10 +405,13 @@ ax = plt.hexbin(
     linewidths=(0.15,)
     #     edgecolors=None
 )
+plt.xlim([0, 12])
+plt.ylim([0, 1800])
 ax = plt.gca()
 ax.plot(x_line, y_line, "--k")
 ax.annotate(
-    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (8, 1490),
+    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
+    (6, 1490),
 )
 _ = ax.set_xlabel("Euclidian Distance of Preprint-Published Versions")
 _ = ax.set_ylabel("Time Elapsed Until Preprint is Published (Days)")
@@ -456,10 +419,6 @@ cbar = plt.colorbar()
 _ = cbar.ax.set_ylabel("count", rotation=270)
 plt.savefig("output/article_distance_vs_publication_time_hex.svg", dpi=250)
 plt.savefig("output/article_distance_vs_publication_time_hex.png", dpi=250)
-
-
-# In[18]:
-
 
 plt.figure(figsize=(6, 5))
 ax = plt.hexbin(
@@ -471,20 +430,20 @@ ax = plt.hexbin(
     mincnt=1,
     edgecolors=None,
 )
+plt.xlim([0, 12])
+plt.ylim([0, 1800])
 ax = plt.gca()
 ax.plot(x_line, y_line, "--k")
 ax.annotate(
-    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (8, 1490),
+    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
+    (6, 1490),
 )
 _ = ax.set_xlabel("Euclidian Distance of Preprint-Published Versions")
 _ = ax.set_ylabel("Time Elapsed Until Preprint is Published (Days)")
 cbar = plt.colorbar()
 _ = cbar.ax.set_ylabel("count", rotation=270)
 
-
-# In[19]:
-
-
+# +
 hexplot = sns.jointplot(
     x="doc_distances",
     y="days_to_published",
@@ -494,7 +453,6 @@ hexplot = sns.jointplot(
     norm=mpl.colors.LogNorm(),
     height=8,
 )
-
 hexplot.set_axis_labels(
     xlabel="Euclidian Distance of Preprint-Published Versions",
     ylabel="Time Elapsed Until Preprint is Published (Days)",
@@ -502,7 +460,8 @@ hexplot.set_axis_labels(
 
 hexplot.ax_joint.plot(x_line, y_line, "--k")
 hexplot.ax_joint.annotate(
-    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (8, 1490),
+    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
+    (6, 1490),
 )
 # shrink fig so cbar is visible
 plt.subplots_adjust(left=0.2, right=0.8, top=0.8, bottom=0.2)
@@ -511,10 +470,7 @@ cbar_ax = hexplot.fig.add_axes([0.85, 0.25, 0.05, 0.4])  # x, y, width, height
 plt.colorbar(cax=cbar_ax)
 _ = cbar_ax.set_ylabel("count", rotation=270)
 
-
-# In[20]:
-
-
+# +
 hexplot = sns.jointplot(
     x="doc_distances",
     y="days_to_published",
@@ -532,7 +488,8 @@ hexplot.set_axis_labels(
 
 hexplot.ax_joint.plot(x_line, y_line, "--k")
 hexplot.ax_joint.annotate(
-    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}", (8, 1490),
+    f"Y={results_2.slope:.2f}*X+{results_2.intercept:.2f}",
+    (6, 1490),
 )
 # shrink fig so cbar is visible
 plt.subplots_adjust(left=0.2, right=0.8, top=0.8, bottom=0.2)
@@ -540,10 +497,3 @@ plt.subplots_adjust(left=0.2, right=0.8, top=0.8, bottom=0.2)
 cbar_ax = hexplot.fig.add_axes([0.85, 0.25, 0.05, 0.4])  # x, y, width, height
 plt.colorbar(cax=cbar_ax)
 _ = cbar_ax.set_ylabel("count", rotation=270)
-
-
-# In[ ]:
-
-
-
-
